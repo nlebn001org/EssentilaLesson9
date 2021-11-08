d84475567460dca6f4cb44056dcc3b6ed742ef1f
Nikita Lebedenko <nlebn001@gmail.com>
2021-11-08 18:11:18 +0100

    Add project files.

---
+++ EssentilaLesson9Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EssentilaLesson9Task1
+{
+
+    //    Задание
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента и возвращает среднее арифметическое этих аргументов.
+
+    class Program
+    {
+
+        public delegate int MyDelegate(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            MyDelegate d = delegate (int a, int b, int c) { return a + b + c; };
+            MyDelegate r = (a, b, c) => (a + b + c) / 3;
+
+            int sum = r(1, 2, 3);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ EssentilaLesson9Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace EssentilaLesson9Task3
+{
+
+    //    Задание 3
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и возвращает среднее арифметическое возвращаемых значений методов, 
+    //    сообщенных с делегатами в массиве.Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int.
+
+    class Program
+    {
+
+        public delegate int MyDel(MyDel[] a);
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        }
+    }
+}
