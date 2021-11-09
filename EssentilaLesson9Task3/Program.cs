047541c5f3f08e471fadb7556cd637a33a063920
Nikita Lebedenko <nlebn001@gmail.com>
2021-11-09 17:22:15 +0100

    3 task

--- EssentilaLesson9Task3/Program.cs
+++ EssentilaLesson9Task3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 
 namespace EssentilaLesson9Task3
 {
@@ -12,14 +13,31 @@ namespace EssentilaLesson9Task3
     class Program
     {
 
-        public delegate int MyDel(MyDel[] a);
+        public delegate int MyDelLast(MyDel[] a);
+        public delegate int MyDel(int a, int b);
 
         static void Main(string[] args)
         {
+            MyDel del = (a, b) => (a + b) / 2;
+            int a = new Random().Next(5,20);
+            MyDel[] delArr = new MyDel[a];
+
+            for (int i = 0; i < delArr.Length; i++)
+            {
+                delArr[i] = del;
+            }
             
 
+            MyDelLast delLast;
+
+            for (int i = 0; i < delArr.Length; i++)
+            {
+                int b = new Random().Next(5, 50);
+                delLast = (delArr) => (delArr[i].Invoke(b, b+5)) / 2;
+                Console.WriteLine(delLast.Invoke(delArr));
+                Thread.Sleep(20);
+            }
         }
 
-        }
     }
 }
