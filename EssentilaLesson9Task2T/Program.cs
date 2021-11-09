0158922cc600720a20cace950f2131a18ffadbc3
Nikita Lebedenko <nlebn001@gmail.com>
2021-11-09 16:42:40 +0100

    new task added

---
+++ EssentilaLesson9Task2T/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EssentilaLesson9Task2T
+{
+    class Program
+    {
+        public delegate double MyDelegate(double a, double b);
+
+
+        static void Main(string[] args)
+        {
+            MyDelegate add = (a, b) => { return a + b; };
+            MyDelegate sub = (a, b) => { return a - b; };
+            MyDelegate mul = (a, b) => { return a * b; };
+            MyDelegate div = (a, b) => { if (a == 0) return 0; else return a / b; };
+
+            int x = 10;
+            int z = 5;
+
+            Console.WriteLine($"1st parameter is {x} and 2nd is{z}");
+            Console.WriteLine($"Sum is {add(x, z)}");
+            Console.WriteLine($"Sub is {add(x, z)}");
+            Console.WriteLine($"Mul is is {add(x, z)}");
+            Console.WriteLine($"Div is {add(x, z)}");
+        }
+    }
+}
